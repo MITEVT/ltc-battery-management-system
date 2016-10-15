053899336e23663873932efbe1028ed5d0a96a95
Skanda Koppula <skanda.koppula@gmail.com>
2016-10-15 14:35:47 -0400

    add spi example

---
+++ src/spi.c
@@ -0,0 +1,113 @@
+
+#include "chip.h"
+
+const uint32_t OscRateIn = 0;
+
+#define SSP_MODE_TEST       1	/*1: Master, 0: Slave */
+#define BUFFER_SIZE                         (0x100)
+#define SSP_DATA_BITS                       (SSP_BITS_8)
+#define SSP_DATA_BIT_NUM(databits)          (databits + 1)
+#define SSP_DATA_BYTES(databits)            (((databits) > SSP_BITS_8) ? 2 : 1)
+#define SSP_LO_BYTE_MSK(databits)           ((SSP_DATA_BYTES(databits) > 1) ? 0xFF : (0xFF >> \
+																					  (8 - SSP_DATA_BIT_NUM(databits))))
+#define SSP_HI_BYTE_MSK(databits)           ((SSP_DATA_BYTES(databits) > 1) ? (0xFF >> \
+																			   (16 - SSP_DATA_BIT_NUM(databits))) : 0)
+#define LPC_SSP           LPC_SSP0
+#define SSP_IRQ           SSP0_IRQn
+#define SSPIRQHANDLER     SSP0_IRQHandler
+
+
+#define LED_PIN 5
+/* Tx buffer */
+static uint8_t Tx_Buf[BUFFER_SIZE];
+
+/* Rx buffer */
+static uint8_t Rx_Buf[BUFFER_SIZE];
+
+static SSP_ConfigFormat ssp_format;
+static Chip_SSP_DATA_SETUP_T xf_setup;
+
+static void Init_SSP_PinMux(void) {
+    Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_2, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* MISO0 */ 
+    Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_3, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* MOSI0 */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_0, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* SSEL0 */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_1, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* SCK0 */
+	Chip_IOCON_PinLocSel(LPC_IOCON, IOCON_SCKLOC_PIO2_11);
+}
+
+static void Buffer_Init(void) {
+	uint16_t i;
+	uint8_t ch = 0;
+
+	for (i = 0; i < BUFFER_SIZE; i++) {
+		Tx_Buf[i] = 0x01;
+		Rx_Buf[i] = 0xAA;
+	}
+} 
+
+static void GPIO_Config(void) {
+	Chip_GPIO_Init(LPC_GPIO);
+
+}
+
+static void LED_Config(void) {
+	Chip_GPIO_WriteDirBit(LPC_GPIO, 2, LED_PIN, true);
+
+}
+
+static void LED_On(void) {
+	Chip_GPIO_SetPinState(LPC_GPIO, 2, LED_PIN, true);
+	//Chip_UART_SendBlocking(LPC_USART, "LED_ON", 6);
+}
+
+static void LED_Off(void) {
+	Chip_GPIO_SetPinState(LPC_GPIO, 2, LED_PIN, false);
+}
+
+/**
+ * @brief	Main routine for SSP example
+ * @return	Nothing
+ */
+int main(void)
+{
+	SystemCoreClockUpdate();
+
+	if (SysTick_Config (SystemCoreClock / 1000)) {
+		//Error
+		while(1);
+	}
+
+	/* LED Initialization */
+	GPIO_Config();
+	LED_Config();
+	LED_On();
+
+
+    Init_SSP_PinMux();
+	Chip_SSP_Init(LPC_SSP);
+	Chip_SSP_SetBitRate(LPC_SSP, 30000);
+	/* SSP initialization */
+	
+
+	ssp_format.frameFormat = SSP_FRAMEFORMAT_SPI;
+	ssp_format.bits = SSP_DATA_BITS;
+	ssp_format.clockMode = SSP_CLOCK_MODE0; 
+	Chip_SSP_SetFormat(LPC_SSP, ssp_format.bits, ssp_format.frameFormat, ssp_format.clockMode);
+	Chip_SSP_SetMaster(LPC_SSP, SSP_MODE_TEST);
+	Chip_SSP_Enable(LPC_SSP);
+
+
+	Buffer_Init();
+
+	LED_On();
+
+	int i;
+
+	while (1) {
+		Chip_SSP_WriteFrames_Blocking(LPC_SSP, Tx_Buf, BUFFER_SIZE);
+		for(i=0; i< 0xFFFF; i++);
+
+	}
+
+	return 0;
+}

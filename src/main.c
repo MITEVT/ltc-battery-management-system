cbc35e2848748abf40efb41e41a78633e20488de
Eric Ponce <eaponce@mit.edu>
2016-10-22 19:04:59 -0400

    Sending SPI messeges

--- src/main.c
+++ src/main.c
@@ -6,25 +6,17 @@
 
 const uint32_t OscRateIn = 0;
 
-#define SSP_MODE_TEST       1	/*1: Master, 0: Slave */
 #define TX_BUFFER_SIZE                         (0x100)
 #define RX_BUFFER_SIZE                         (0x100)
-#define SSP_DATA_BITS                       (SSP_BITS_8)
-#define SSP_DATA_BIT_NUM(databits)          (databits + 1)
-#define SSP_DATA_BYTES(databits)            (((databits) > SSP_BITS_8) ? 2 : 1)
-#define SSP_LO_BYTE_MSK(databits)           ((SSP_DATA_BYTES(databits) > 1) ? 0xFF : (0xFF >> \
-																					  (8 - SSP_DATA_BIT_NUM(databits))))
-#define SSP_HI_BYTE_MSK(databits)           ((SSP_DATA_BYTES(databits) > 1) ? (0xFF >> \
-																			   (16 - SSP_DATA_BIT_NUM(databits))) : 0)
+
 #define LPC_SSP           LPC_SSP1
 #define SSP_IRQ           SSP1_IRQn
 #define SSPIRQHANDLER     SSP1_IRQHandler
 
-#define _cs_pin 	2
-#define _cs_port 	3
+#define CS 0, 7
 
 
-#define LED_PIN 5
+#define LED0 2, 10
 /* Tx buffer */
 static uint8_t Tx_Buf[TX_BUFFER_SIZE];
 
@@ -47,48 +39,14 @@ static char str[100];
 static SSP_ConfigFormat ssp_format;
 static Chip_SSP_DATA_SETUP_T xf_setup;
 
-static void Init_SSP_PinMux(void) {
-    Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_2, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* MISO1 */ 
-    Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_3, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* MOSI1 */
-	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_0, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* SSEL1 */ // WILL NEED TO USE _cs pin manually in future
-	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_1, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* SCK1 */
-
-	// Chip_GPIO_WriteDirBit(LPC_GPIO, _cs_port, _cs_pin, true);
-	Chip_IOCON_PinLocSel(LPC_IOCON, IOCON_SCKLOC_PIO2_11);
-}
-
-static void Buffer_Init(void) {
-	uint16_t i;
-
-    Tx_Buf[0] = 0x00;
-    Tx_Buf[1] = 0x01;
-    // Hard coding PEC, use erpo's PEC code for later
-    Tx_Buf[2] = 0x3D;
-    Tx_Buf[2] = 0x6E;
-
-	for (i = 0; i < RX_BUFFER_SIZE; i++) {
-		Rx_Buf[i] = 0xAA;
-	}
-} 
-
 static void GPIO_Config(void) {
 	Chip_GPIO_Init(LPC_GPIO);
 
 }
 
 static void LED_Config(void) {
-	Chip_GPIO_WriteDirBit(LPC_GPIO, 2, LED_PIN, true);
+	Chip_GPIO_WriteDirBit(LPC_GPIO, LED0, true);
 
-}
-
-static void LED_On(void) {
-	Chip_GPIO_SetPinState(LPC_GPIO, 2, LED_PIN, true);
-	Chip_UART_SendBlocking(LPC_USART, "LED_ON", 6);
-}
-
-static void LED_Off(void) {
-	Chip_GPIO_SetPinState(LPC_GPIO, 2, LED_PIN, false);
-	Chip_UART_SendBlocking(LPC_USART, "LED_OFF", 6);
 }
 
 static void LTC8604_RDCFG(void){
@@ -128,23 +86,6 @@ int main(void)
 	GPIO_Config();
 	LED_Config();
 
-    /*
-	Chip_GPIO_WriteDirBit(LPC_GPIO, 2, 2, true);
-	Chip_GPIO_WriteDirBit(LPC_GPIO, 2, 3, true);
-    */
-
-    /*
-    while(1) {
-        if(msTicks % 10 == 0) {
-	        Chip_GPIO_SetPinState(LPC_GPIO, 2, 2, true);
-	        Chip_GPIO_SetPinState(LPC_GPIO, 2, 3, true);
-        }
-        if(msTicks % 10 == 5) {
-	        Chip_GPIO_SetPinState(LPC_GPIO, 2, 2, false);
-	        Chip_GPIO_SetPinState(LPC_GPIO, 2, 3, false);
-        }
-    }*/
-
 	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_6, (IOCON_FUNC1 | IOCON_MODE_INACT));/* RXD */
 	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_7, (IOCON_FUNC1 | IOCON_MODE_INACT));/* TXD */
 
@@ -154,36 +95,39 @@ int main(void)
 	Chip_UART_SetupFIFOS(LPC_USART, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2));
 	Chip_UART_TXEnable(LPC_USART);
 
-    Init_SSP_PinMux();
 	Chip_SSP_Init(LPC_SSP);
 	Chip_SSP_SetBitRate(LPC_SSP, 30000);
-	/* SSP initialization */
-	
 
 	ssp_format.frameFormat = SSP_FRAMEFORMAT_SPI;
-	ssp_format.bits = SSP_DATA_BITS;
-	ssp_format.clockMode = SSP_CLOCK_MODE0;  // may need to change
+	ssp_format.bits = SSP_BITS_8;
+	ssp_format.clockMode = SSP_CLOCK_MODE3;  // may need to change
 	Chip_SSP_SetFormat(LPC_SSP, ssp_format.bits, ssp_format.frameFormat, ssp_format.clockMode);
 	Chip_SSP_SetMaster(LPC_SSP, true);
 	Chip_SSP_Enable(LPC_SSP);
 
 
-	Buffer_Init();
+	Chip_GPIO_SetPinState(LPC_GPIO, LED0, true);
 
-	LED_On();
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_2, (IOCON_FUNC2 | IOCON_MODE_INACT));	/* MISO1 */ 
+    Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_3, (IOCON_FUNC2 | IOCON_MODE_INACT));	/* MOSI1 */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_1, (IOCON_FUNC2 | IOCON_MODE_INACT));	/* SCK1 */
+	// Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_9, (IOCON_FUNC0 | IOCON_MODE_INACT));
+	Chip_GPIO_WriteDirBit(LPC_GPIO, CS, true);
 
+	uint16_t i;
 
-    /*
-	int i;
-	Chip_UART_SendBlocking(LPC_USART, "Sending Reading Config Register:\n", 25);
-	LED_Off();
-	for(i = 0; i<6;i++){
-		itoa(Rx_Buf[i],str,16);
-		Chip_UART_SendBlocking(LPC_USART,str, strlen(str));
-	}*/
+    Tx_Buf[0] = 0x00;
+    Tx_Buf[1] = 0x01;
+    // Hard coding PEC, use erpo's PEC code for later
+    Tx_Buf[2] = 0x3D; // 0011 1101
+    Tx_Buf[3] = 0x6E;
 
 	while(1) {
-	    LTC8604_RDCFG();
+		Chip_GPIO_SetPinState(LPC_GPIO, CS, false);
+	    Chip_UART_SendBlocking(LPC_USART, "sending frames..\n",17);	
+		Chip_SSP_WriteFrames_Blocking(LPC_SSP, Tx_Buf, 4);
+		Chip_UART_SendBlocking(LPC_USART, "done sending frames..\n",23);
+		Chip_GPIO_SetPinState(LPC_GPIO, CS, true);
     }
 
 	return 0;

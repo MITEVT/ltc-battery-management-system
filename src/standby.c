9288de742e9c37595ec3cf2fcfee16115af81614
Skanda Koppula <skoppula@mit.edu>
2017-01-07 15:04:17 -0500

    add standby mode

---
+++ src/standby.c
@@ -0,0 +1,14 @@
+#include "standby.h"
+
+void Standby_Init(BMS_STATE_T *state) {
+}
+
+void Standby_Config(BMS_STATE_T *state) {
+}
+
+void Standby_Step(BMS_INPUT_T *input, BMS_STATE_T *state, BMS_OUTPUT_T *output) {
+}
+
+void Standby_Reset(BMS_STATE_T *state){
+}
+

0330bd198cef4083258c9146b1d79347a84027fa
Jorge Troncoso <jatron@mit.edu>
2017-03-11 20:21:38 -0800

    Implemented CellTemperatures_Step(). Still need to implement helper functions called by CellTemperatures_Step()

--- inc/cell_temperatures.h
+++ inc/cell_temperatures.h
@@ -4,6 +4,42 @@
 // ltc-battery-controller
 #include "state_types.h"
 
+// constants
+#define NUMBER_OF_MULTIPLEXER_LOGIC_CONTROL_INPUTS 5
+
+
+/****************************************************************************************
+ * Private Functions
+ * *************************************************************************************/
+
+/**
+ * @details get address of current thermistor
+ *
+ * @param currentThermistor value of current thermistor
+ * @param thermistorAddress array of bits representing the address of currentThermistor
+ */
+void getThermistorAddress(uint8_t currentThermistor, uint8_t * thermistorAddress);
+
+/**
+ * @details sets address of multiplexer by controlling shit register
+ *
+ * @param thermistorAddress array of bits representing the address of a thermistor
+ */
+void setMultiplexerAddress(uint8_t * thermistorAddress);
+
+/**
+ * @details get voltage of currentThermistor
+ *
+ * @param currentThermistor value of current thermistor
+ * @param pack_status datatype containing mutable array of cell temperatures
+ */
+void getThermistorVoltage(uint8_t currentThermistor, BMS_PACK_STATUS_T * pack_status);
+
+
+/****************************************************************************************
+ * Public Functions
+ * *************************************************************************************/
+
 /**
  * @details get cell temperature of next thermistor
  *
--- src/cell_temperatures.c
+++ src/cell_temperatures.c
@@ -1,11 +1,57 @@
 // ltc-battery-controller
 #include "cell_temperatures.h"
 #include "state_types.h"
+#include "config.h"
 
 //lpc11cx4-library
 #include "lpc_types.h"
 
-void CellTemperatures_Step(BMS_PACK_STATUS_T * pack_status) {
-	UNUSED(pack_status);
+// state
+static uint8_t currentThermistor = 0;
+
+/**************************************************************************************
+ * Private Functions
+ * ***********************************************************************************/
+
+void getThermistorAddress(uint8_t currentThermistor, uint8_t * thermistorAddress) {
+	UNUSED(currentThermistor);
+	UNUSED(thermistorAddress);
 	// TODO
+}
+
+void setMultiplexerAddress(uint8_t * thermistorAddress) {
+	UNUSED(thermistorAddress);
+	// TODO
+}
+
+void getThermistorVoltage(uint8_t currentThermistor, BMS_PACK_STATUS_T * pack_status) {
+	
+	// get thermistor address
+	uint8_t thermistorAddress[NUMBER_OF_MULTIPLEXER_LOGIC_CONTROL_INPUTS];	
+	getThermistorAddress(currentThermistor, thermistorAddress);
+
+	// set multiplexer address
+	setMultiplexerAddress(thermistorAddress);
+
+	UNUSED(pack_status);
+	
+	// TODO: read thermistor voltage and store result in pack_status
+}
+
+
+/**************************************************************************************
+ * Public Functions
+ * ***********************************************************************************/
+
+void CellTemperatures_Step(BMS_PACK_STATUS_T * pack_status) {
+
+	// move to next thermistor
+	if (currentThermistor < MAX_THERMISTORS_PER_MODULE) {
+		currentThermistor += 1;
+	} else {
+		currentThermistor = 0;
+	}
+
+	// get thermistor voltage
+	getThermistorVoltage(currentThermistor, pack_status);
 }

230293f951d29b7e2e99fb1d3f522b92bb0ae177
alec-heif <alec.heif@gmail.com>
2017-04-17 20:22:49 -0400

    Continue eviscerating board.c

---
+++ inc/evt_drivers/evt_can.h
@@ -0,0 +1,12 @@
+#ifndef _EVT_CAN_H
+#define _EVT_CAN_H
+
+#include <stdint.h>
+
+#include "state_types.h"
+
+void Evt_Can_Init(uint32_t baudRateHz);
+void Evt_Can_Receive(BMS_INPUT_T *bms_input, BMS_OUTPUT_T *bms_output);
+void Evt_Can_Transmit(BMS_INPUT_T *bms_input, BMS_STATE_T *bms_state, BMS_OUTPUT_T *bms_output);
+
+#endif
--- src/board.c
+++ src/board.c
@@ -13,7 +13,7 @@
 
 #else // FSAE_DRIVERS
 
-    #include "can.h"
+    #include "evt_can.h"
     #include "brusa.h"
 
 #endif // FSAE_DRIVERS
@@ -62,13 +62,6 @@ uint8_t currentThermistor = 0;
 //Cell temperature sensing stuff
 static bool ltc6804_setMultiplexerAddressFlag = false;
 static bool ltc6804_getThermistorVoltagesFlag = false;
-
-#else //FSAE_DRIVERS
-
-//CAN STUFF
-CCAN_MSG_OBJ_T can_rx_msg;
-
-static uint32_t _last_brusa_ctrl = 0;
 
 #endif // FSAE_DRIVERS
 
@@ -198,7 +191,7 @@ void Board_CAN_Init(uint32_t baudRateHz){
 #ifdef FSAE_DRIVERS
     Fsae_Can_Init(baudRateHz);
 #else // FSAE_DRIVERS
-    CAN_Init(baudRateHz);
+    Evt_Can_Init(baudRateHz);
 #endif // FSAE_DRIVERS
 
 #else
@@ -741,34 +734,7 @@ void Board_CAN_ProcessInput(BMS_INPUT_T *bms_input, BMS_OUTPUT_T *bms_output) {
     Fsae_Can_Receive(bms_input, bms_output);
     return;
 #else // FSAE_DRIVERS
-    CCAN_MSG_OBJ_T rx_msg;
-    if (CAN_Receive(&rx_msg) != NO_RX_CAN_MESSAGE) {
-        if (rx_msg.mode_id == NLG5_STATUS) { 
-            // [TODO] use info from brusa message
-        } else if (rx_msg.mode_id == NLG5_ACT_I) {
-            NLG5_ACT_I_T act_i;
-            Brusa_DecodeActI(&act_i, &rx_msg);
-            bms_input->pack_status->pack_current_mA = act_i.output_cAmps*10; // [TODO] Consider using current sense as well
-            bms_input->pack_status->pack_voltage_mV = act_i.output_mVolts;
-
-            // If current > requested current + thresh throw error
-        } else if (rx_msg.mode_id == NLG5_ACT_II) {
-
-        } else if (rx_msg.mode_id == NLG5_TEMP) {
-
-        } else if (rx_msg.mode_id == NLG5_ERR && bms_output->charge_req->charger_on) {
-            // [TODO] distinguish errors
-            if (!Brusa_CheckErr(&rx_msg)) { // We've recevied a Brusa Error Message
-                // if (output->charge_req->charger_on) {
-                    Error_Assert(ERROR_BRUSA, msTicks);
-                // }
-                // We should try to clear but also assert error for count
-                // Timing idea: Brusa error msg happens as often as ctrl message
-            } else {
-                Error_Pass(ERROR_BRUSA);
-            }
-        }
-    }
+    Evt_Can_Receive(bms_input, bms_output);
 #endif // FSAE_DRIVERS
 }
 
@@ -777,47 +743,7 @@ void Board_CAN_ProcessOutput(BMS_INPUT_T *bms_input, BMS_STATE_T * bms_state, BM
     Fsae_Can_Transmit(bms_input, bms_state, bms_output);
     return;
 #else // FSAE_DRIVERS
-
-    UNUSED(bms_state);
-
-    // [TODO] Make timing.h that has this (or board.h)
-    // Make python script generate
-    #define NLG5_CTL_DLY_mS 99
-
-    // Easy way to turn off charger in case of accident
-    if (bms_output->charge_req->charger_on && msTicks - _last_brusa_ctrl >= NLG5_CTL_DLY_mS) {
-        NLG5_CTL_T brusa_control;
-        CCAN_MSG_OBJ_T temp_msg;
-        brusa_control.enable = 1;
-        brusa_control.ventilation_request = 0;
-        brusa_control.max_mains_cAmps = 1000; // [TODO] Magic Numbers
-        brusa_control.output_mV = bms_output->charge_req->charge_voltage_mV;
-        brusa_control.output_cA = bms_output->charge_req->charge_current_mA / 10;
-        const ERROR_STATUS_T * stat = Error_GetStatus(ERROR_BRUSA);
-        if (stat->handling) {
-            brusa_control.clear_error = stat->count & 1;
-            brusa_control.output_mV = 0;
-            brusa_control.output_cA = 0;
-            bms_input->charger_on = false;
-        } else {
-             brusa_control.clear_error = 0;
-             bms_input->charger_on = true;
-        }
-
-        Brusa_MakeCTL(&brusa_control, &temp_msg);
-        CAN_TransmitMsgObj(&temp_msg);
-        _last_brusa_ctrl = msTicks;
-    }
-
-    if (!bms_output->charge_req->charger_on) {
-        bms_input->charger_on = false;
-    }
-    if (CAN_GetErrorStatus()) {
-        Board_Println("CAN Error");
-        Error_Assert(ERROR_CAN, msTicks);
-        CAN_ResetPeripheral();
-        Board_CAN_Init(CAN_BAUD);
-    }
+    Evt_Can_Transmit(bms_input, bms_state, bms_output);
 #endif // FSAE_DRIVERS
 }
 #endif // TEST_HARDWARE

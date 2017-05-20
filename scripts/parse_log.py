6275b70818b1361319951771fab6ba8d63bc3041
Jorge Troncoso <jatron@mit.edu>
2017-05-20 16:57:06 -0400

    Implemented code that saves pack voltage to a csv file

--- scripts/parse_log.py
+++ scripts/parse_log.py
@@ -1,14 +1,15 @@
 import sys
 import re
 
-if len(sys.argv) != 5:
-    print("Requires four arguments: read-file-log.txt voltages-filename.csv temps-filename.csv pack_current-filename.csv")
+if len(sys.argv) != 6:
+    print("Requires five arguments: read-file-log.txt voltages-filename.csv temps-filename.csv pack_current-filename.csv pack_voltage-filename.csv")
     sys.exit(1)
 
 read_file = sys.argv[1]
 voltages_file = sys.argv[2]
 temps_file = sys.argv[3]
 pack_current_file = sys.argv[4]
+pack_voltage_file = sys.argv[5]
 
 with open(read_file, 'r') as f:
     lines = f.readlines()
@@ -22,10 +23,13 @@ def get_voltages(parts):
 def get_temps(parts):
     vals = []
     for part in parts[3:]:
-        vals.append(int(part.strip())/100.0)
+        vals.append(int(part.strip())/10.0)
     return vals
 
 def get_pack_current(parts):
+    return parts[2]
+
+def get_pack_voltage(parts):
     return parts[2]
 
 def get_module(parts):
@@ -47,6 +51,7 @@ def get_time(parts):
 voltage_lines = []
 temp_lines = []
 pack_current_lines = []
+pack_voltage_lines = []
 
 for line in lines:
     line = line.strip()
@@ -56,7 +61,7 @@ for line in lines:
     line = line.strip()
     parts = line.split(",")
     if len(parts) < 3: continue
-    if (not (parts[1]=='csv' or parts[1]=='temps' or parts[1]=='pcurr')): continue
+    if (not (parts[1]=='cvs' or parts[1]=='temps' or parts[1]=='pcurr' or parts[1]=='pvolt')): continue
     time = get_time(parts)
     if parts[1] == 'cvs':
         module = get_module(parts)
@@ -72,11 +77,16 @@ for line in lines:
         pack_current = get_pack_current(parts);
         line = str(time) + "," + str(pack_current)
         pack_current_lines.append(line)
+    elif parts[1] == 'pvolt':
+        pack_voltage = get_pack_voltage(parts)
+        line = str(time) + "," + str(pack_voltage)
+        pack_voltage_lines.append(line)
 
 
 print(temp_lines)
 print(voltage_lines)
 print(pack_current_lines)
+print(pack_voltage_lines)
 with open(temps_file, 'w') as f:
     print("Writing temperatures to file",temps_file)
     for line in temp_lines:
@@ -89,3 +99,7 @@ with open(pack_current_file, 'w') as f:
     print("Writing pack current to file", pack_current_file)
     for line in pack_current_lines:
         f.write(line + "\n")
+with open(pack_voltage_file, 'w') as f:
+    print("Writing pack voltage to file", pack_voltage_file)
+    for line in pack_voltage_lines:
+        f.write(line +"\n")
